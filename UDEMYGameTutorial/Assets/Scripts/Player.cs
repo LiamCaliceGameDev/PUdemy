addcc8c984b3e1884794e11266d5c17d89db8e2c
BTP <liamcalicegamedev@gmail.com>
2018-10-06 15:15:31 +0200

    Initial Commit
    course progress

---
+++ UDEMYGameTutorial/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    private Vector2 moveAmount;
+    private Animator anim;
+
+    public int health;
+
+    public GameObject[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        UpdateHealthUI(health);
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ChangeWeapon(Weapon weaponToEquip) {
+        Destroy(GameObject.FindGameObjectWithTag("Weapon"));
+        Instantiate(weaponToEquip, transform.position, transform.rotation, transform);
+    }
+
+    void UpdateHealthUI(int currentHealth) {
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+
+            if (i < currentHealth)
+            {
+                hearts[i].GetComponent<Image>().sprite = fullHeart;
+            } else {
+                hearts[i].GetComponent<Image>().sprite = emptyHeart;
+            }
+
+        }
+
+    }
+
+    public void Heal(int healAmount) {
+        if (health + healAmount > 5)
+        {
+            health = 5;
+        } else {
+            health += healAmount;
+        }
+        UpdateHealthUI(health);
+    }
+
+
+}
---
+++ UDEMYGameTutorial/Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public float lifeTime;
+    public int damage;
+
+    public GameObject explosion;
+
+    private void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile() {
+        Destroy(gameObject);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+         if (other.tag == "Enemy")
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectile();
+        }
+    }
+
+
+}
